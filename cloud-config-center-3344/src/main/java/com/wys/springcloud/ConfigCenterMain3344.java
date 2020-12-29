0ec2ea34a3f2f3a29fa0c9b3e3233fe778a60758
wys <1640230150@qq.com>
2020-12-29 14:24:36 +0800

    20.12.29 14.24

---
+++ cloud-config-center-3344/src/main/java/com/wys/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.wys.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: wys
+ * @Description: TODO
+ * @DateTime: 2020/12/29 14:05
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
